aa3b08590b2af8f701616841e2bb7386f449f876
jb1361 <justinbutler4@hotmail.com>
2018-10-27 23:53:31 -0400

    Initialize a random array for the data

--- src/app/modules/util/component/algorithm-graph/algorithm-graph.component.ts
+++ src/app/modules/util/component/algorithm-graph/algorithm-graph.component.ts
@@ -6,33 +6,34 @@ import CanvasJS from '../../../../../assets/canvasjs.min';
   styleUrls: ['./algorithm-graph.component.scss']
 })
 export class AlgorithmGraphComponent implements OnInit {
-  @Input() data: number[];
+  @Input() data: {};
   @Input() algorithm: string;
+  @Input() size: number;
   constructor() { }
 
   ngOnInit() {
+    if (!this.data) {
+      this.initializeData(this.size ? this.size : 25);
+    }
+    CanvasJS.addColorSet('blue', ['#0000b2']);
     const chart = new CanvasJS.Chart('chartContainer', {
       animationEnabled: true,
       exportEnabled: false,
       interactivityEnabled: false,
       zoomEnabled: false,
       panEnabled: false,
+      colorSet: 'blue',
+      dataPointMaxWidth: 20,
+      width: (this.size * 20),
+      axisX: {
+        interval: 1
+      },
       title: {
         text: this.algorithm,
       },
       data: [{
         type: 'column',
-        dataPoints: [
-          { y: 71, label: 'Apple' },
-          { y: 55, label: 'Mango' },
-          { y: 50, label: 'Orange' },
-          { y: 65, label: 'Banana' },
-          { y: 95, label: 'Pineapple' },
-          { y: 68, label: 'Pears' },
-          { y: 28, label: 'Grapes' },
-          { y: 34, label: 'Lychee' },
-          { y: 14, label: 'Jackfruit' }
-        ]
+        dataPoints: this.data
       }]
     });
 
@@ -40,5 +41,26 @@ export class AlgorithmGraphComponent implements OnInit {
   }
   public runAlgorithm() {
   }
+  public  initializeData(size: number) {
+    const data = [];
+    for (let i = 1; i <= size; i++) {
+      data.push({x: i, y: i});
+    }
+    this.size = size;
+    // this.data = data;
+    this.data = this.shuffle(data);
+  }
+ public shuffle(array) {
+   for (let i = array.length - 1; i >= 0; i--) {
+     const randomIndex = Math.floor(Math.random() * (i + 1));
+     const itemAtIndex = array[randomIndex];
+     array[randomIndex] = array[i];
+     array[randomIndex]['x'] = array[i]['x'];
+     array[i] = itemAtIndex;
+     array[i]['x'] = i;
+   }
+    return array;
+  }
+
 
 }
